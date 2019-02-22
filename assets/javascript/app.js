ffaba07b1796268a0f3c7b7e8f7d19d5d6270cad
Treudey <alextreude@hotmail.com>
2019-02-22 13:12:55 -0500

    Added end screen and reset button as well as one media query and a couple more questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,24 +17,45 @@ var q1 = new Question(
     "./assets/images/julius-death.gif");
 
 var q2 = new Question(
-    'Who was the first man to walk on the moon?', 
-    ['Neil Armstrong',
-    'Buzz Aldrin',
-    'Alan Shepard',
-    'John Glenn'],
-    0, 
+    'Who created one of the largest empires of the ancient world in 323 BC by the age of thirty?', 
+    ['Achilles',
+    'Constantine the Great',
+    'Alaxander the Great',
+    'Augustus Caesar'],
+    2, 
     "./assets/images/alexander.gif");
 
-questionList = [q1, q2];
-index = 0;
-correct = 0;
-incorrect = 0;
-unanswered = 0;
-currentQuestion = questionList[index];
+var q3 = new Question(
+    'Who wrote the two epic poems the <i>Iliad<i/> and the <i>Odyssey<i/>?', 
+    ['Homer',
+    'Aristotle',
+    'Hesiod',
+    'Virgil'],
+    0, 
+    "./assets/images/homer.gif");
 
+var q4 = new Question(
+    'What was Rome\'s largest amphitheatre?', 
+    ['The Circus Maximus',
+    'The Colosseum',
+    'The Theatre of Marcellus',
+    'The Pantheon'],
+    1, 
+    "./assets/images/colosseum.gif");
+
+var q5 = new Question(
+    'Of the Seven Wonders of the Ancient World, which is the only one still standing today?', 
+    ['The Great Pyramid of Giza',
+    'The Temple of Artemis at Ephesus',
+    'The Statue of Zeus at Olympia',
+    'The Lighthouse of Alexandria'],
+    0, 
+    "./assets/images/pyramids.gif");
+
+questionList = [q1, q2, q3, q4, q5];
 
 Question.prototype.displayQuestion = function() {
-    $("#question").text(this.question);
+    $("#question").html(this.question);
     for (var i = 0; i < this.answers.length; i++) {
         $("#answer" + i).text(this.answers[i]);
     }
@@ -51,6 +72,15 @@ Question.prototype.incorrectGuess = function() {
 
 Question.prototype.checkIfCorrect = function(guess) {
     return guess === this.answers[this.correctAnswer];
+}
+
+function initialize() {
+    index = 0;
+    correct = 0;
+    incorrect = 0;
+    unanswered = 0;
+    currentQuestion = questionList[index];
+    $(".title").after('<button class="start-button">START</button>');
 }
 
 function createQuestionPage() {
@@ -78,46 +108,74 @@ function timer() {
         guessResult(timeLeft);
     }
 }
+
 function createResultPage(result) {
     $(".answers").remove();
     $("<img>").appendTo(".question-div");
-    if (result === "correct") {
+    if (result === true) {
         $("#question").text("Correct!");
         currentQuestion.correctGuess();
     } else {
         $("#question").after('<p class="incorrect-text">The Correct Answer was: <span class="incorrect-text" id="correct-answer"></span></p>');
         currentQuestion.incorrectGuess();
-        if (result === "incorrect") {
+        if (result === false) {
             $("#question").text("Incorrect!");
-        } else if (result === "out of time") {
+        } else if (result === 0) {
             $("#question").text("Out of Time!");
         }
     }
 }
 
+function createStatsPage() {
+    $("#time-remaining").remove();
+    $("#question").remove();
+    $(".incorrect-text").remove();
+    $("img").remove();
+    $(".question-div").append('<h2 class="end-text">All done! Here\'s how you did:</h2>');
+    $(".question-div").append('<p class="stats">Correct Answers: ' + correct + '</p>');
+    $(".question-div").append('<p class="stats">Incorrect Answers: ' + incorrect + '</p>');
+    $(".question-div").append('<p class="stats">Unanswered: ' + unanswered + '</p>');
+    $(".question-div").append('<button class="reset-button">START OVER?</button>');
+}
+
 function guessResult(guess) {
     clearInterval(questionTimer);
     if (guess === 0) {
-        createResultPage("out of time");
+        createResultPage(guess);
         unanswered++;
     } else if (currentQuestion.checkIfCorrect(guess)) {
-        createResultPage("correct");
+        createResultPage(true);
         correct++;
     } else {
-        createResultPage("incorrect");
+        createResultPage(false);
         incorrect++;
     }
     index++;
-    currentQuestion = questionList[index];
-    setTimeout(createQuestionPage, 5000);
+    if (index > questionList.length - 1) {
+        setTimeout(createStatsPage, 5000);
+    } else {
+        currentQuestion = questionList[index];
+        setTimeout(createQuestionPage, 5000);
+    }
 }
 
-$(document).on("click", "#start", function() {
-    $(".question-div").empty();
-    createQuestionPage();
-});
+$(document).ready(function() {
 
-$(document).on("click", ".answers", function() {
-    var guess = $(this).text();
-    guessResult(guess);
-});
+    initialize();
+
+    $(document).on("click", ".start-button", function() {
+        $(this).remove();
+        createQuestionPage();
+    });
+    
+    $(document).on("click", ".answers", function() {
+        var guess = $(this).text();
+        guessResult(guess);
+    });
+    
+    $(document).on("click", ".reset-button", function() {
+        $(".question-div").empty();
+        initialize();
+    });
+})
+
